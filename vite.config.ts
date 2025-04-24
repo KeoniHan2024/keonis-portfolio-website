a5d6ee8341dbb111e42736d8b20da6e93d993f89
KeoniHan2024 <keonihan@gmail.com>
2025-04-23 14:34:22 -1000

    fixed url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/keoni-portfolio-website",
+  base: "/keonis-portfolio-website/",
 })
