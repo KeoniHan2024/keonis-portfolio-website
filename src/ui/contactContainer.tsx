c62ca7d7e9ca31d8bf10313e80e6d49b6742049b
KeoniHan2024 <keonihan@gmail.com>
2025-04-24 22:34:10 -1000

    Update contactContainer.tsx

--- src/ui/contactContainer.tsx
+++ src/ui/contactContainer.tsx
@@ -42,7 +42,7 @@ function ContactContainer() {
                 </div>
                 <div className="bio-row">
                     <p className='bio-text'>
-                        I'm a 23 year-old software developer from Honolulu, HI. I have been programming for about 5 years.
+                        I'm a software developer from Honolulu, HI. I have been programming for about 5 years.
                     </p>
                 </div>
             </div>
